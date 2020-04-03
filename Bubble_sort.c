bbca8566feea8fabe0bef77e099694134c201568
PMUP <2071635368@qq.com>
2020-04-04 02:51:59 +0800

    OK

--- Bubble_sort.c
+++ Bubble_sort.c
@@ -5,26 +5,27 @@ int main(void)
         char ch = 0;
 	int len = 0;
 	int s = 0;
-	int arr[9999] = {0};
-	printf("输入一些数字(以空格隔开)：");
-	while(scanf("%d", &arr[s]))
+	int arr[999] = {0};
+	while(1)
 	{
-		printf("<");
-		if(getchar() == '\n')
+		if(scanf("%d", &arr[s]) == EOF)
 			break;
+		printf("<A> ");
 		s ++;
 		len ++;
+		if(getchar() == '\n')
+			break;
 	}
 	if(len >= 2)
 	{
-		for(int i = 0; i < len; i ++)
+		for(int i = 0; i < len -1; i ++)
 		{
-			for(int k = 0; k < len - i; k ++)
+			for(int k = 0; k < len -1 -i; k ++)
 			{
-				if(arr[k] > arr[k+1])
+				if(arr[k] > arr[k +1])
 				{
-					int t = arr[k+1];
-					arr[k+1] = arr[k];
+					int t = arr[k +1];
+					arr[k +1] = arr[k];
 					arr[k] = t;
 				}
 				else
@@ -36,8 +37,9 @@ int main(void)
 	}
 	for(int i = 0; i < len; i ++)
 	{
-		printf("%d", arr[i]);
+		printf("%d\t", arr[i]);
 	}
+	printf("\n");
 
 	return 0 ;
 }
