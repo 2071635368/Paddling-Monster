27ff3d505b5ff415a0e084d1fd14180d7c06405f
PMUP <61903325+2071635368@users.noreply.github.com>
2020-04-06 18:32:36 +0800

    Update recusrion_sort.c

--- recusrion_sort.c
+++ recusrion_sort.c
@@ -1,83 +1,92 @@
 # include <stdio.h>
+# include <malloc.h>
 
-void spilt(int* arr, int len)
+void spilt(int* arr,int low, int lin, int len)
 {
-	int IN_len = len / 2 + (len % 2);
-	int L_arr[IN_len];
-	int R_arr[len];
-	for(int i = 0; i < IN_len; i ++)
+	int LIN = lin + 1;
+	int* L_arr[lin - low];
+	int* R_arr[len - lin];
+
+	for (int i = low; i <= lin; i++)
 	{
-		L_arr[i] = arr[i];
+		L_arr[i - low] = arr[i];
 	}
-	for(int i = IN_len; i < len; i ++)
+	for (int i = LIN; i <= len; i++)
 	{
-		R_arr[i] = arr[i];
+		R_arr[i - LIN] = arr[i];
 	}
 
-	int j = 0, k = IN_len, m = 0;
-	while(j < IN_len && k < len)
+	int j = 0, k = 0, m = 0;
+	while (j <= (lin-low) && k <= (len - LIN))
 	{
-		if(L_arr[j] < R_arr[k])
+		if (L_arr[j] < R_arr[k])
 		{
 			arr[m] = L_arr[j];
-			j ++;
-			m ++;
-			if(j == IN_len)
-			{
-				while(k < len)
-				{
-					arr[m] = R_arr[k];
-					k ++;
-					m ++;
-				}		
-			}
-		}	
+			j++;
+			m++;
+		}
 		else
 		{
 			arr[m] = R_arr[k];
-			k ++;
-			m ++;
-			if(k == len)
-			{
-				while(j < IN_len)
- 				{
-					arr[m] = L_arr[j];
-					j ++;
-					m ++;
-				}
-			}
+			k++;
+			m++;
 		}
 	}
+	while (j > (lin - low) || k > (len - LIN))
+	{
+		if (j > (lin - low))
+		{
+			while (k <= (len - LIN))
+			{
+				arr[m] = R_arr[k];
+					k++;
+					m++;
+			}
+		}
+		else if(k > (len - LIN))
+		{
+			while (j <= (lin - low))
+			{
+				arr[m] = L_arr[j];
+				j++;
+				m++;
+			}
+		}
+		break;
+	}
+	printf("%d | %d", arr[0], arr[1]);
 }
 
-void sort(int* arr, int IN_len, int len)
+void sort(int* arr,int LOW_len, int UP_len)
 {
-	if(IN_len == 0 || len == IN_len)
+	if (LOW_len >= UP_len)
 	{
-		return;	
+		return;
 	}
 
-	sort(arr, len, IN_len --);
-	sort(arr, IN_len, len --);	
-	spilt(arr, len);
+	int LIN_len = (LOW_len + UP_len) / 2;
+
+	sort(arr, LOW_len, LIN_len);
+
+	sort(arr, LIN_len+1, UP_len);
+	
+	spilt(arr, LOW_len, LIN_len, UP_len);
 }
 
 void merge_sort(int* arr, int len)
-{
-	int IN_len = len / 2 + (len % 2);
-	sort(arr, IN_len, len);
+{	
+	sort(arr, 0, len);
 }
 
 int main(void)
 {
-	int arr[10] = {5, 1, 2, 7, 10, 3, 6, 9, 4, 8};
-	merge_sort(arr, 10);
+	int arr[10] = {100, 5, 20};
+	merge_sort(arr, 3);
 
-	for(int i = 0; i < 10; i ++)
+	for (int i = 0; i < 3; i++)
 	{
 		printf("%d\n", arr[i]);
 	}
 
 	return 0;
 }
-
