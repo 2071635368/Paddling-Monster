25212040bd03530f8b0c76311ee6d8a3f41ed3f4
PMUP <2071635368@qq.com>
2020-04-25 12:09:39 +0800

    meger-sort Features

--- tool/stges.c
+++ tool/stges.c
@@ -1,5 +1,9 @@
 /*
-	统计输入路径的文件中每个字符出现的次数
+	完成时间: 2020.4.25 / 12:05
+	作者: 2071635368
+
+	作用: 统计输入路径的文件中每个字符出现的次数
+
 	使用：
 		    生成的可执行文件 路径
 */
@@ -20,7 +24,10 @@ typedef struct cnlist{
 }CNS;
 
 int juge(FILE*, char*, char*);
-void traverse_list(CNS*);
+int traverse_list(CNS*);
+void sort(CNS*, int, int, int);
+void meger(CNS*, int, int);
+void meger_sort(CNS*);
 
 void screen(FILE* in, CNS* cns){
 	char buff;
@@ -78,6 +85,7 @@ int main(int argc, char** argv){
 	}
 	CNS cns = {{0}, 0, NULL};
 	screen(in, &cns);
+	meger_sort(&cns);
 	traverse_list(&cns);
 	
 	fclose(in);
@@ -131,11 +139,116 @@ int juge(FILE* in, char* buff, char* tmp){
 	return time;
 }
 
-void traverse_list(CNS* cns){
+void meger_sort(CNS* cns){
+	int len = 0;
 	CNS* cnt = cns;
+	while(cnt != NULL){
+		cnt = cnt->pnext;
+		len ++;
+	}
+	meger(cns, 0, len-1);
+}
+
+void meger(CNS* cns, int low, int len){
+	if(low >= len){
+		return;
+	}
+	int mid = (low + len) / 2;
+	meger(cns, low, mid);
+	mid ++;
+	meger(cns, mid, len);
+	sort(cns, low, mid, len);
+}
+
+void sort(CNS* cns, int low, int mid, int len){
+	CNS* tlow = cns;
+	CNS* tmid = cns;
+	CNS* tlen = cns;
+	for(int i = 0; i < low; i ++){
+		tlow = tlow->pnext;
+	}
+	for(int i = 0; i < mid; i ++){
+		tmid = tmid->pnext;
+	}
+	for(int i = 0; i < len; i ++){
+		if(tlen->pnext != NULL){
+			tlen = tlen->pnext;
+		}
+	}
+	
+	int ls = mid - low;
+	int rs = len - mid + 1;
+	CNS* lcns = (CNS*)malloc(sizeof(CNS) * ls);
+	CNS* rcns = (CNS*)malloc(sizeof(CNS) * rs);
+
+	int i = 0;
+	CNS* tmp = tlow;
+	while(tmp != tmid){
+		strcpy(lcns[i].cr, tmp->cr);
+		lcns[i].weight = tmp->weight;
+		tmp = tmp->pnext;
+		i ++;
+	}
+
+	i = 0;
+	tmp = tmid;
+	while(tmp != tlen){
+		strcpy(rcns[i].cr, tmp->cr);
+		rcns[i].weight = tmp->weight;
+		tmp = tmp->pnext;
+		i ++;
+	}
+	if(tmp == tlen){
+		strcpy(rcns[i].cr, tmp->cr);
+		rcns[i].weight = tmp->weight;
+	}
+	
+	i = 0;
+	tmp = tlow;
+	int j = 0, k = 0;
+	while(j < ls && k < rs && tmp->pnext != NULL){
+		if(lcns[j].weight < rcns[k].weight || lcns[j].weight == rcns[k].weight){
+			strcpy(tmp->cr, lcns[j].cr);
+			tmp->weight = lcns[j].weight;
+			tmp = tmp->pnext;
+			j ++;
+		}
+		else{
+			strcpy(tmp->cr, rcns[k].cr);
+			tmp->weight = rcns[k].weight;
+			tmp = tmp->pnext;
+			k ++;
+		}
+	}
+
+	while(j < ls || k < rs && tmp->pnext != NULL){
+		if(j < ls){
+			strcpy(tmp->cr, lcns[j].cr);
+			tmp->weight = lcns[j].weight;
+			tmp = tmp->pnext;
+			j ++;
+		}
+		else{
+			strcpy(tmp->cr, rcns[k].cr);
+			tmp->weight = rcns[k].weight;
+			tmp = tmp->pnext;
+			k ++;
+		}
+	}
+	
+	free(lcns);
+	free(rcns);
+}
+
+int traverse_list(CNS* cns){
+	CNS* cnt = cns;
+	int i = 0;
 	while(cnt != NULL){
 		printf("字符> %s的权值为: ", cnt->cr);
 		printf("%d\n", cnt->weight);
 		cnt = cnt->pnext;
+		i ++;
 	}
+	printf("\nPs: Carriage return is the real carriage return !\n");
+	return i;
 }
